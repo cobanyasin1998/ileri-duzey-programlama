018f5c24c60d64a678bc5d955b1645243f5dc575
Yasin Çoban <cobanyasin1998@gmail.com>
2024-02-04 23:44:03 +0300

    exception handling ve dinamik nesne üretimi

---
+++ 1x4-exceptionhandling/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+while (true)
+{
+    var key = Console.ReadKey();
+    if (key.Key == ConsoleKey.R)
+    {
+        throw new CustomException();
+    }
+}
+
+class CustomException : Exception
+{
+
+    public CustomException(string message) : base(message)
+    {
+    }
+    public CustomException() : base("HATA")
+    {
+
+    }
+
+}
---
+++ 1x5-dinamik-nesne-uretimi/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+#region Activator Sınıfı ile Nesne Oluşturma
+using System.Dynamic;
+
+{
+    Type objectType = Type.GetType("MyClass");
+
+    MyClass instance = (MyClass)Activator.CreateInstance(objectType);
+}
+#endregion
+
+#region DynamicObject Sınıfı ile Nesne Üretme
+{
+    dynamic product = new Product();
+    product.Oylesine = 1;
+    product.TrySetMember("Name", "Laptop");
+    product.TrySetMember("Price", 5000);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.Oylesine);
+
+}
+#endregion
+
+#region dynamic keyword'ü ile Nesne Üretme
+{
+    dynamic product = new ExpandoObject();
+    product.Name = "Laptop";
+    product.Price = 5000;
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+}
+#endregion
+
+public class MyClass
+{
+    public MyClass()
+    {
+        Console.WriteLine($"{nameof(MyClass)} instance created");
+    }
+    public int MyProperty { get; set; } = 5;
+}
+
+public class Product : DynamicObject
+{
+    public Product()
+    {
+        Console.WriteLine($"{nameof(Product)} instance created");
+    }
+    private readonly Dictionary<string, object> properties = new();
+    public override bool TrySetMember(SetMemberBinder binder, object? value)
+    {
+        properties.Add(binder.Name, value);
+        return true;
+    }
+    public override bool TryGetMember(GetMemberBinder binder, out object? result)
+    {
+
+        return properties.TryGetValue(binder.Name, out result);
+    }
+}
