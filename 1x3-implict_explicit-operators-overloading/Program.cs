56ec8c145fbf8493fc797cd768dab8bc55ef3f96
Yasin Çoban <cobanyasin1998@gmail.com>
2024-02-04 18:42:50 +0300

    tür dönüşümleri

---
+++ 1x3-implict_explicit-operators-overloading/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Implicit(Kapalı) and Explicit(Açık) operators
+
+A a1 = new B();
+A a2 = (A)new B();
+
+B b = (B)new A();
+class A
+{
+    public int Name { get; set; }
+    public static implicit operator A(B b)
+    {
+        return new A()
+        {
+            Name = b.Title
+        };
+    }
+}
+
+class B
+{
+    public int Title { get; set; }
+    public static explicit operator B(A a)
+    {
+        return new B()
+        {
+            Title = a.Name
+        };
+    }
+}
