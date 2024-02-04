d55bfcec8c74e24a1d6be7aeef0f91de209977cf
Yasin Çoban <cobanyasin1998@gmail.com>
2024-02-04 17:09:35 +0300

    Add project files.

---
+++ 1x0-diziler/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+//Dizilerde Derinlemesine İnceleme
+
+//Tek boyutlu dizi tanımlama
+int[] array = new int[10];
+
+//Çok boyutlu dizi tanımlama
+int[,,] arrays = new int[3, 3, 3]
+{
+    {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    },
+    {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    },
+    {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+     }
+};
+
+//Matris tanımlama
+int[,] matrix = new int[3, 3]
+{
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+
+//Düzensiz Diziler (Jagged Arrays) tanımlama
+int[][] jaggedArray = new int[3][];
+jaggedArray[0] = new int[2] { 1, 2 };
+jaggedArray[1] = new int[3] { 1, 2, 3 };
+jaggedArray[2] = new int[4] { 1, 2, 3, 4 };
+
+
+//Createinstance metodu ile dizi oluşturma
+
+int[] d1 = (int[])Array.CreateInstance(typeof(int), 2);
+int[,,] d2 = (int[,,])Array.CreateInstance(typeof(int), 2, 2,2);
+
+
+//Dizilerde Yeni Syntax Kullanımı
+
+(int a,string b)[] tupleArray = new (int a, string b)[] 
+{ 
+    (1, "bir"), 
+    (2, "iki"), 
+    (3, "üç") 
+};
---
+++ 1x1-refReturn-refLocal/Program.cs
@@ -0,0 +1,79 @@
+﻿
+//Ref keyword ne işe yarar?
+//Ref keyword, bir metoda bir değişkenin referansını göndermek için kullanılır.
+
+
+using System.Security.Cryptography.X509Certificates;
+
+int b = 5;
+X(ref b);
+Console.WriteLine(b); // 10
+void X(ref int a)
+{
+    a = 10;
+}
+//Ref Return keyword ne işe yarar?
+//Ref Return keyword, bir metottan bir değişkenin referansını döndürmek için kullanılır.
+
+int ab = 5;
+ref int _ab = ref XY(ref ab);
+
+Console.WriteLine(ab); // 10
+
+ref int XY(ref int ab)
+{
+    ab = 10;
+    return ref ab;
+}
+
+//Ref return - Kritik 1
+
+int _a = 4;
+var __a = A(ref _a);
+
+Console.WriteLine(_a); // 10
+
+ref int A(ref int _a)
+{
+    _a = 10;
+    return ref _a;
+}
+
+//Ref return - Kritik 2
+
+int _b = 4;
+var __b = B(ref _b);
+Console.WriteLine(_b);
+
+ref int B(ref int _b)//parametreden farklı bir değişkenin referansını döndürdüğümüzde hata alırız.
+{
+    int _c = 10;
+    _b = 10;
+    // return ref _c; // _c'nin referansını döndürdüğü için hata alırız.
+    return ref _b;
+}
+
+//Örnek 
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+
+ref int numberRef = ref FindElement(numbers, 3);
+
+ref int FindElement(int[] array, int target)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == target)
+        {
+            return ref array[i];
+        }
+    }
+    throw new IndexOutOfRangeException($"{nameof(target)} not found");
+
+}
+
+//Ref Local keyword ne işe yarar?
+//Ref Local keyword, bir değişkenin referansını tutmak için kullanılır.
+
+char _o = 'o';
+ref char p = ref _o;
